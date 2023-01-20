0d62af165bb6ddc5fb160d4edf26e091a44c7e42
Osomhe1 <osomhe.aleogho290@gmail.com>
2023-01-20 02:55:57 +0100

    carousel update

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,7 @@ module.exports = {
     // './pages/_app.js/**/*.{js,ts,jsx,tsx}',
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
+    './node_modules/flowbite-react/**/*.js',
   ],
   theme: {
     extend: {
@@ -26,6 +27,6 @@ module.exports = {
       },
     },
   },
-  plugins: [],
+  plugins: [require('flowbite/plugin')],
   // })
 }
